4ceccb7561d8c34c90eafe78b306a3c999abb80e
Serani <delvinlim6414@gmail.com>
2024-10-09 12:59:46 +0800

    half done, but need to change bootstrap to better reflect stuff

--- app.py
+++ app.py
@@ -4,15 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
-
-@app.route('/about')
-def about():
-    return render_template("about.html")
+    return render_template("index.html", pageTitle="About")
 
 @app.route('/tours')
 def tours():
-    return render_template('tours.html')
+    return render_template('tours.html', pageTitle="Tours", totalNumberOfTours=10)
 
 if __name__ == '__main__':
     app.run(debug=True)
